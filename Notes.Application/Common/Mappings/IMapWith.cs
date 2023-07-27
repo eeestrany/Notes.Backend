fe79b6409da74992b3bbd9f743a43d30df0bdc87
eeestrany <yaroslavprotsyk310@gmail.com>
2023-07-27 16:31:58 +0300

    Add project files.

---
+++ Notes.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+using System.Runtime;
+
+namespace Notes.Application.Common.Mappings
+{
+    public interface IMapWith<T>
+    {
+        void Mapping(Profile profile) => 
+            profile.CreateMap(typeof(T), GetType());
+    }
+}
